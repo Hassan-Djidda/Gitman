8dfdec63bfb1ebc8cb93de10001045eb90362a75
Hassan Mahamat Djidda <87423039+Hassan-Djidda@users.noreply.github.com>
2023-06-30 19:06:25 +0100

    Add content  dolly.sh

--- dolly.sh
+++ dolly.sh
@@ -1,1 +1,20 @@
 
+git clone git clone file:///tmp/dolly # To clone 
+git checkout -b dev-mul
+git branch
+
+cd dolly
+ls 
+cat main.py
+
+echo "def mul(num1, num2):" >> main.py
+echo "    return num1 * num2" >> main.py
+cat main.py
+git status
+
+git add main.py
+git commit -m "New function multiplication"
+
+git checkout main
+git merge dev-mul
+git branch -d dev-mul
