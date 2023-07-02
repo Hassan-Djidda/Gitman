ec2f44cea5ee345cd99d8ff1fdb655e77b9e248c
Hassan Mahamat Djidda <87423039+Hassan-Djidda@users.noreply.github.com>
2023-07-02 16:29:47 +0100

    Update dolly.sh

--- dolly.sh
+++ dolly.sh
@@ -1,10 +1,10 @@
 git clone git clone file:///tmp/dolly # To clone a project 
 git checkout -b dev-mul # Create a branch and Switched on
-git branch
+git branch # To list All my branch
 
-cd dolly
-ls 
-cat main.py
+cd dolly # Channge the directory to dolly
+ls  # List the content of the current directory 
+cat main.py # Print the content of main,py
 
 echo "def mul(num1, num2):" >> main.py
 echo "    return num1 * num2" >> main.py
